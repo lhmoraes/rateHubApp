445b403aa898cbd5274aca0995ea5b2e1710179a
Luiz Moraes <luiz.moraes@rbc.com>
2018-06-12 15:32:33 -0400

    Added reponsive support

--- main.bundle.js
+++ main.bundle.js
@@ -20,14 +20,14 @@ webpackEmptyAsyncContext.id = "./src/$$_lazy_route_resource lazy recursive";
 /***/ "./src/app/app.component.css":
 /***/ (function(module, exports) {
 
-module.exports = "\r\n\r\n*{ \r\n    font-family: Roboto;\r\n    text-align: center;\r\n}\r\n\r\nhtml, body {\r\n  background: #333;\r\n  height: 100%;\r\n  overflow: hidden;\r\n  text-align: center;\r\n}\r\n\r\n.svg-wrapper {\r\n  height: 60px;\r\n\tmargin: 0 auto;\r\n  position: relative;\r\n  top: 50%;\r\n  -webkit-transform: translateY(-50%);\r\n          transform: translateY(-50%);\r\n  width: 320px;\r\n  cursor: pointer;\r\n}\r\n\r\n.shape {\r\n  fill: transparent;\r\n  stroke-dasharray: 140 540;\r\n  stroke-dashoffset: -474;\r\n  stroke-width: 8px;\r\n  stroke: #ff4000;\r\n}\r\n\r\n.text {\r\n  color: black;\r\n  font-size: 22px;\r\n  letter-spacing: 8px;\r\n  line-height: 32px;\r\n  position: relative;\r\n  top: -48px;\r\n}\r\n\r\n@-webkit-keyframes draw {\r\n  0% {\r\n    stroke-dasharray: 140 540;\r\n    stroke-dashoffset: -474;\r\n    stroke-width: 8px;\r\n  }\r\n  100% {\r\n    stroke-dasharray: 760;\r\n    stroke-dashoffset: 0;\r\n    stroke-width: 2px;\r\n  }\r\n}\r\n\r\n@keyframes draw {\r\n  0% {\r\n    stroke-dasharray: 140 540;\r\n    stroke-dashoffset: -474;\r\n    stroke-width: 8px;\r\n  }\r\n  100% {\r\n    stroke-dasharray: 760;\r\n    stroke-dashoffset: 0;\r\n    stroke-width: 2px;\r\n  }\r\n}\r\n\r\n.svg-wrapper:hover .shape {\r\n  -webkit-animation: 0.5s draw linear forwards;\r\n  animation: 0.5s draw linear forwards;\r\n}\r\n\r\n#heading{\r\n  color:#D5DDE5;;\r\n  background:#1b1e24;\r\n  border-bottom:4px solid #9ea7af;\r\n  border-right: 1px solid #343a45;\r\n  font-size:23px;\r\n  font-weight: 100;\r\n  padding:24px;\r\n  text-align:left;\r\n  text-shadow: 0 1px 1px rgba(0, 0, 0, 0.1);\r\n  vertical-align:middle;\r\n}\r\n\r\n#body{\r\n  border-top: 1px solid #C1C3D1;\r\n  border-bottom: 1px solid #C1C3D1;\r\n  color:#666B85;\r\n  font-size:16px;\r\n  font-weight:normal;\r\n  text-shadow: 0 1px 1px rgba(256, 256, 256, 0.1);\r\n}\r\n\r\nimg{ \r\n    width: 70%;\r\n    padding-top: 0%; \r\n}\r\n\r\n"
+module.exports = "\r\nmat-drawer-container  {\r\n  padding-top: 0;\r\n}\r\n\r\nmat-drawer{\r\n  background: #0e83cd;\r\n}\r\n\r\n@media screen and (max-width: 992px) { \r\nmat-drawer{ \r\n  width: 90%;\r\n  background: #0e83cd;\r\n}\r\n\r\n\r\n\r\n}\r\n\r\n.topHeading{ \r\n  padding-top: 2%;\r\n  margin-top: 0;\r\n  color: white;\r\n}\r\n\r\n.topHeading h2{ \r\n  margin: 0;\r\n  padding: 0;\r\n}\r\n\r\n.navButton{ \r\n    background: none;\r\n    cursor: pointer;\r\n    padding: 25px 80px;\r\n    display: inline-block;\r\n    margin: 15px 30px;\r\n    font-weight: 500;\r\n    -webkit-transition: all 0.3s;\r\n    transition: all 0.3s;\r\n    border: 3px solid #fff;\r\n    color: #fff;\r\n}\r\n\r\n.navButton:hover{ \r\n  background-color: white;\r\n  color: #0e83cd;\r\n}\r\n\r\n.navIconWrap{ \r\n  margin: 0%;\r\n  padding: 2%;\r\n  background: #0e83cd;\r\n  color: white;\r\n}\r\n\r\n.navBar {\r\n    width: 35px;\r\n    height: 5px;\r\n    background-color: white;\r\n    margin: 6px 0;\r\n}\r\n\r\n.tableWrap{ \r\n}\r\n\r\n#table{ \r\n  text-align: center;\r\n  min-width: 600px;\r\n  overflow-x:auto; \r\n /* box-shadow: 10px 10px 5px #0e83cd; */\r\n}\r\n\r\n#heading{ \r\n  border-bottom: 2px solid #0e83cd;\r\n  padding: 2%;\r\n  font-weight: bold;\r\n}\r\n\r\n.tableData{ \r\n  padding: 2%;\r\n  border-bottom: 1px solid #86c1e6;\r\n}\r\n\r\n.tableData:hover{ \r\n  background: #b6d9f0;\r\n}\r\n\r\nimg{ \r\n  width: 25%;\r\n}\r\n\r\n/*\r\n\r\n.example-container {\r\n  width: 500px;\r\n  height: 300px;\r\n  border: 1px solid rgba(0, 0, 0, 0.5);\r\n}\r\n\r\n.example-sidenav-content {\r\n  display: flex;\r\n  height: 100%;\r\n  align-items: center;\r\n  justify-content: center;\r\n}\r\n\r\n.example-sidenav {\r\n  padding: 20px;\r\n}\r\n\r\n*{ \r\n    font-family: Roboto;\r\n    text-align: center;\r\n}\r\n\r\nhtml, body {\r\n  background: #333;\r\n  height: 100%;\r\n  overflow: hidden;\r\n  text-align: center;\r\n}\r\n\r\n.divTable{ \r\n  overflow-x:auto;\r\n}\r\n\r\n#mobileNav div{ \r\n  width: 100%;\r\n  background: ghostwhite;\r\n  padding: 3%;\r\n  border-bottom: 2px solid gainsboro;\r\n}\r\n\r\n#mobileNav div:hover{ \r\n  background: gainsboro;\r\n}\r\n\r\n.svg-wrapper {\r\n  height: 60px;\r\n\tmargin: 0 auto;\r\n  position: relative;\r\n  top: 50%;\r\n  transform: translateY(-50%);\r\n  width: 320px;\r\n  cursor: pointer;\r\n}\r\n\r\n.shape {\r\n  fill: transparent;\r\n  stroke-dasharray: 140 540;\r\n  stroke-dashoffset: -474;\r\n  stroke-width: 8px;\r\n  stroke: #ff4000;\r\n}\r\n\r\n.text {\r\n  color: black;\r\n  font-size: 22px;\r\n  letter-spacing: 8px;\r\n  line-height: 32px;\r\n  position: relative;\r\n  top: -48px;\r\n}\r\n\r\n@keyframes draw {\r\n  0% {\r\n    stroke-dasharray: 140 540;\r\n    stroke-dashoffset: -474;\r\n    stroke-width: 8px;\r\n  }\r\n  100% {\r\n    stroke-dasharray: 760;\r\n    stroke-dashoffset: 0;\r\n    stroke-width: 2px;\r\n  }\r\n}\r\n\r\n.svg-wrapper:hover .shape {\r\n  -webkit-animation: 0.5s draw linear forwards;\r\n  animation: 0.5s draw linear forwards;\r\n}\r\n\r\n#heading{\r\n  color:#D5DDE5;;\r\n  background:#1b1e24;\r\n  border-bottom:4px solid #9ea7af;\r\n  border-right: 1px solid #343a45;\r\n  font-size:23px;\r\n  font-weight: 100;\r\n  padding:24px;\r\n  text-align:left;\r\n  text-shadow: 0 1px 1px rgba(0, 0, 0, 0.1);\r\n  vertical-align:middle;\r\n}\r\n\r\n#body{\r\n  border-top: 1px solid #C1C3D1;\r\n  border-bottom: 1px solid #C1C3D1;\r\n  color:#666B85;\r\n  font-size:16px;\r\n  font-weight:normal;\r\n  text-shadow: 0 1px 1px rgba(256, 256, 256, 0.1);\r\n}\r\n\r\n\r\n\r\nimg{ \r\n    width: 70%;\r\n    padding-top: 0%; \r\n}\r\n\r\n*/"
 
 /***/ }),
 
 /***/ "./src/app/app.component.html":
 /***/ (function(module, exports) {
 
-module.exports = "<div id=\"nav\" fxLayout=\"row\">\n  <div fxFlex=\"25%\">  \n    <div (click)=\"seniorClick()\" class=\"svg-wrapper\">\n      <svg height=\"60\" width=\"320\" xmlns=\"http://www.w3.org/2000/svg\">\n        <rect class=\"shape\" height=\"60\" width=\"320\" />\n      </svg>\n      <div class=\"text\"> Senior Accounts </div>\n    </div>\n  </div>\n  <div fxFlex=\"25%\">  \n    <div (click)=\"taxFreeClick()\" class=\"svg-wrapper\">\n      <svg height=\"60\" width=\"320\" xmlns=\"http://www.w3.org/2000/svg\">\n        <rect class=\"shape\" height=\"60\" width=\"320\" />\n      </svg>\n      <div class=\"text\"> Tax Free Accounts </div>\n    </div>\n  </div>\n  <div fxFlex=\"25%\">  \n    <div (click)=\"highInterstClick()\" class=\"svg-wrapper\">\n      <svg height=\"60\" width=\"320\" xmlns=\"http://www.w3.org/2000/svg\">\n        <rect class=\"shape\" height=\"60\" width=\"320\" />\n      </svg>\n      <div class=\"text\"> High Interest </div>\n    </div>\n  </div>\n  <div fxFlex=\"10%\">  \n    <div (click)=\"youthClick()\" class=\"svg-wrapper\">\n      <svg height=\"60\" width=\"320\" xmlns=\"http://www.w3.org/2000/svg\">\n        <rect class=\"shape\" height=\"60\" width=\"320\" />\n      </svg>\n      <div class=\"text\"> Youth Accounts </div>\n    </div>\n  </div>\n  \n</div>\n\n<br> <br> \n\n<div id =\"container\" fxLayoutAlign=\"row\">\n\n  <div fxFlex=\"10%\"></div>\n\n  <div fxFlex=\"80%\">\n\n    <h1> {{ heading }} </h1>\n\n    <div id=\"heading\"  fxLayout=\"row\">\n     <!-- <div *ngFor=\"let place of seniorAccounts\"> -->\n\n              <div fxFlex=\"10%\"> Logo </div>\n              <div fxFlex=\"25%\"> Broker Name </div>\n              <div fxFlex=\"15%\"> Location </div>\n              <div fxFlex=\"25%\"> Interest Rate </div>\n              <div fxFlex=\"25%\"> Type Of Account </div>\n      <!--</div>-->\n\n    </div>\n\n    <div id=\"body\" *ngFor=\"let place of viewable\" fxLayout=\"row\">\n     <!-- <div *ngFor=\"let place of seniorAccounts\"> -->\n          \n            <div fxFlex=\"10%\"> <img src={{place.brokerLogo}}/> </div>\n             <div fxFlex=\"25%\">  <br> {{ place.brokerName }} </div>\n              <div fxFlex=\"15%\"> <br> {{place.location}} </div>\n              <div fxFlex=\"25%\"> <br> {{place.interestRate}}% </div>\n              <div fxFlex=\"25%\"> <br> {{place.type}} </div>\n      <!--</div>-->\n\n    </div>\n\n  </div>\n\n</div>\n\n\n\n"
+module.exports = "<mat-drawer-container [style.height.px]=\"pageHeight\" class=\"container\" >\n  <mat-drawer #drawer class=\"sidenav\" opened=\"true\" mode=\"side\">\n    \n    <div class=\"topHeading\" fxLayout=\"row\" fxLayoutAlign=\"center center\">\n      <h2> Bank Rates </h2>\n    </div>\n    \n    <div fxLayout=\"row\">\n      <div fxFlex=\"5%\"></div>\n        <div fxFlex=\"90%\"> <button (click)=\"seniorClick();drawer.toggle()\" class=\"navButton\"> Senior Accounts </button> </div>\n    </div>\n    <div fxLayout=\"row\">\n      <div fxFlex=\"5%\"></div>\n      <div fxFlex=\"90%\"> <button (click)=\"taxFreeClick();drawer.toggle()\" class=\"navButton\"> Tax Free Accounts </button> </div>\n    </div>\n    <div fxLayout=\"row\">\n      <div fxFlex=\"5%\"></div>\n      <div fxFlex=\"90%\"> <button (click)=\"highInterstClick();drawer.toggle()\" class=\"navButton\"> High Interest </button> </div>\n    </div>\n    <div fxLayout=\"row\">\n      <div fxFlex=\"5%\"></div>\n      <div fxFlex=\"90%\"> <button (click)=\"youthClick();drawer.toggle()\" class=\"navButton\"> Youth Accounts </button> </div>\n    </div>\n\n  </mat-drawer>\n\n  <div flex layout=\"column\" class=\"sidenav-content\" >\n    \n    <div class=\"navIconWrap\" (click)=\"drawer.toggle()\">\n      <div class=\"navBar\"></div>\n      <div class=\"navBar\"></div>\n      <div class=\"navBar\"></div>\n    </div>\n\n    <div fxLayout=\"row\">\n      <div fxFlex=\"10%\"></div>\n      <div fxFlex=\"80%\">\n        <h1> {{ heading }} </h1>\n      </div>\n    </div>\n\n\n    <!--\n    <mat-table #table [dataSource]=\"viewable\" [dataSource]=\"dataSource\" class=\"mat-elevation-z8\">\n      <ng-container matColumnDef=\"position\">\n        <th mat-header-cell *matHeaderCellDef> No. </th>\n        <td mat-cell *matCellDef=\"let place\"> {{place.brokerName}} </td>\n      </ng-container>\n    </mat-table>\n    -->\n    <div fxLayout=\"row\">\n      <div fxFlex=\"10%\"></div>\n      <div id=\"table\" fxFlex=\"80%\">\n        <div id=\"heading\" fxLayout=\"row\">\n          <p fxFlex=\"20%\"> Logo </p>\n          <p fxFlex=\"20%\"> Name </p>\n          <p fxFlex=\"20%\"> Location </p>\n          <p fxFlex=\"20%\"> Interest Rate </p>\n          <p fxFlex=\"20%\"> Account Type </p>\n        </div>\n        <div class=\"tableData\" fxLayout=\"row\" *ngFor=\"let place of viewable\">\n          <div fxFlex=\"20%\"> <img src=\"{{place.brokerLogo}}\"> </div>\n          <div fxFlex=\"20%\"> {{place.brokerName}} </div>\n          <div fxFlex=\"20%\"> {{place.location}}</div>\n          <div fxFlex=\"20%\"> {{place.interestRate}}% </div>\n          <div fxFlex=\"20%\"> {{place.type}}</div>\n        </div>\n      </div>\n    </div>\n\n\n  </div>\n\n\n\n</mat-drawer-container>"
 
 /***/ }),
 
@@ -52,23 +52,18 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 
 
 var AppComponent = /** @class */ (function () {
-    /*
-      selected= {
-        selectedHighInterest : true,
-        selectedTaxFree : false,
-        selectedSeniorAccounts : false,
-        selectedYouthAccounts : false
-      }
-    */
     function AppComponent(apiClient) {
         var _this = this;
         this.apiClient = apiClient;
         this.title = 'app';
+        this.mobile = false;
         this.searchable = [];
         this.seniorAccounts = [];
         this.taxFree = [];
         this.highInterest = [];
         this.youthAccounts = [];
+        this.pageHeight = 0;
+        this.pageHeight = window.screen.height;
         apiClient.getHighInterest().subscribe(function (data) {
             data.map(function (place) {
                 var currMap = new __WEBPACK_IMPORTED_MODULE_2__data_model_place__["a" /* Place */](place.brokerName, place.brokerLogo, place.location, place.interestRate, place.returnAmount, place.type, place.createdData);
@@ -100,6 +95,8 @@ var AppComponent = /** @class */ (function () {
         this.viewable = this.seniorAccounts;
         this.heading = "Senior Accounts";
     }
+    AppComponent.prototype.ngOnInit = function () {
+    };
     AppComponent.prototype.seniorClick = function () {
         this.viewable = this.seniorAccounts;
         this.heading = "Senior Accounts";
@@ -145,8 +142,12 @@ var AppComponent = /** @class */ (function () {
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_5__app_component__ = __webpack_require__("./src/app/app.component.ts");
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_6__services_api_client_service__ = __webpack_require__("./src/app/services/api-client.service.ts");
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_7__services_component_link_service__ = __webpack_require__("./src/app/services/component-link.service.ts");
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_8__display_table_display_table_component__ = __webpack_require__("./src/app/display-table/display-table.component.ts");
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_9__search_view_search_view_component__ = __webpack_require__("./src/app/search-view/search-view.component.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_8__angular_material_sidenav__ = __webpack_require__("./node_modules/@angular/material/esm5/sidenav.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_9__angular_material_button__ = __webpack_require__("./node_modules/@angular/material/esm5/button.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_10__angular_material_table__ = __webpack_require__("./node_modules/@angular/material/esm5/table.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_11__angular_platform_browser_animations__ = __webpack_require__("./node_modules/@angular/platform-browser/esm5/animations.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_12__display_table_display_table_component__ = __webpack_require__("./src/app/display-table/display-table.component.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_13__search_view_search_view_component__ = __webpack_require__("./src/app/search-view/search-view.component.ts");
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -163,21 +164,29 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 
 
 
+
+
+
+
 var AppModule = /** @class */ (function () {
     function AppModule() {
     }
     AppModule = __decorate([
-        Object(__WEBPACK_IMPORTED_MODULE_1__angular_core__["E" /* NgModule */])({
+        Object(__WEBPACK_IMPORTED_MODULE_1__angular_core__["H" /* NgModule */])({
             declarations: [
                 __WEBPACK_IMPORTED_MODULE_5__app_component__["a" /* AppComponent */],
-                __WEBPACK_IMPORTED_MODULE_8__display_table_display_table_component__["a" /* DisplayTableComponent */],
-                __WEBPACK_IMPORTED_MODULE_9__search_view_search_view_component__["a" /* SearchViewComponent */]
+                __WEBPACK_IMPORTED_MODULE_12__display_table_display_table_component__["a" /* DisplayTableComponent */],
+                __WEBPACK_IMPORTED_MODULE_13__search_view_search_view_component__["a" /* SearchViewComponent */]
             ],
             imports: [
                 __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__["a" /* BrowserModule */],
+                __WEBPACK_IMPORTED_MODULE_11__angular_platform_browser_animations__["a" /* BrowserAnimationsModule */],
                 __WEBPACK_IMPORTED_MODULE_3__angular_forms__["a" /* FormsModule */],
                 __WEBPACK_IMPORTED_MODULE_4__angular_common_http__["b" /* HttpClientModule */],
-                __WEBPACK_IMPORTED_MODULE_2__angular_flex_layout__["a" /* FlexLayoutModule */]
+                __WEBPACK_IMPORTED_MODULE_2__angular_flex_layout__["a" /* FlexLayoutModule */],
+                __WEBPACK_IMPORTED_MODULE_8__angular_material_sidenav__["a" /* MatSidenavModule */],
+                __WEBPACK_IMPORTED_MODULE_9__angular_material_button__["a" /* MatButtonModule */],
+                __WEBPACK_IMPORTED_MODULE_10__angular_material_table__["a" /* MatTableModule */]
             ],
             providers: [
                 __WEBPACK_IMPORTED_MODULE_7__services_component_link_service__["a" /* ComponentLinkService */],
@@ -352,19 +361,19 @@ var ApiClientService = /** @class */ (function () {
         this.httpClient = httpClient;
     }
     ApiClientService.prototype.getSeniorAccounts = function () {
-        return this.httpClient.get("http://ec2-52-43-83-228.us-west-2.compute.amazonaws.com/accounts/seniors-accounts/rates");
+        return this.httpClient.get("http://ec2-52-43-83-228.us-west-2.compute.amazonaws.com/api/accounts/seniors-accounts/rates");
     };
     ApiClientService.prototype.getTaxFree = function () {
-        return this.httpClient.get("http://ec2-52-43-83-228.us-west-2.compute.amazonaws.com/accounts/tax-free/rates");
+        return this.httpClient.get("http://ec2-52-43-83-228.us-west-2.compute.amazonaws.com/api/accounts/tax-free/rates");
     };
     ApiClientService.prototype.getHighInterest = function () {
-        return this.httpClient.get("http://ec2-52-43-83-228.us-west-2.compute.amazonaws.com/accounts/high-interest/rates");
+        return this.httpClient.get("http://ec2-52-43-83-228.us-west-2.compute.amazonaws.com/api/accounts/high-interest/rates");
     };
     ApiClientService.prototype.getYouthAccounts = function () {
-        return this.httpClient.get("http://ec2-52-43-83-228.us-west-2.compute.amazonaws.com/accounts/youth-accounts/rates");
+        return this.httpClient.get("http://ec2-52-43-83-228.us-west-2.compute.amazonaws.com/api/accounts/youth-accounts/rates");
     };
     ApiClientService = __decorate([
-        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["w" /* Injectable */])(),
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["z" /* Injectable */])(),
         __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_1__angular_common_http__["a" /* HttpClient */]])
     ], ApiClientService);
     return ApiClientService;
@@ -406,7 +415,7 @@ var ComponentLinkService = /** @class */ (function () {
         this.searchTable.next(params);
     };
     ComponentLinkService = __decorate([
-        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["w" /* Injectable */])()
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["z" /* Injectable */])()
     ], ComponentLinkService);
     return ComponentLinkService;
 }());
@@ -445,7 +454,7 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 
 
 if (__WEBPACK_IMPORTED_MODULE_3__environments_environment__["a" /* environment */].production) {
-    Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["_8" /* enableProdMode */])();
+    Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["_12" /* enableProdMode */])();
 }
 Object(__WEBPACK_IMPORTED_MODULE_1__angular_platform_browser_dynamic__["a" /* platformBrowserDynamic */])().bootstrapModule(__WEBPACK_IMPORTED_MODULE_2__app_app_module__["a" /* AppModule */])
     .catch(function (err) { return console.log(err); });
